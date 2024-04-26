2f19676d52d3a7a499dc7a018c0e2d55512fdec7
Popsyynn <popsyynn@gmail.com>
2024-04-26 09:48:04 -0700

    Wrote a controller test method for savedEmployee

--- src/test/java/com/example/EmployeeControllerTest.java
+++ src/test/java/com/example/EmployeeControllerTest.java
@@ -1,20 +1,19 @@
 package com.example;
 
-import ch.qos.logback.core.status.Status;
 import com.example.controller.EmployeeController;
 import com.example.entity.Employee;
-import com.example.service.EmployeeService;
+import com.example.exception.EmployeeException;
 import com.example.service.EmployeeServiceImp;
-import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,8 +21,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(EmployeeController.class)
@@ -58,5 +60,24 @@ public class EmployeeControllerTest {
 
 
     }
+
+    @Test
+    public void testSavedEmployee() throws EmployeeException, Exception {
+        Employee employee = new Employee();
+        employee.setId(1l);
+        employee.setFirstName("Popoola");
+        employee.setLastName("Habib");
+        employee.setEmail("popoola@gmail.com");
+        String employeeJson = """
+                {
+                    "id":1 , "firstName":"Popoola"
+                        , "lastName":"Habib", "email":"popoola@gmail.com"
+                                                                            }
+                        """;
+        when(service.createEmployee(Mockito.any(Employee.class))).thenReturn(employee);
+        mvc.perform((MockMvcRequestBuilders.post("/employee").contentType(MediaType.APPLICATION_JSON).content(employeeJson)))
+                .andExpect(status().isCreated()).andReturn();
+    }
+
 
 }
