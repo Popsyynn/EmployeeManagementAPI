e6e5b245c61d8c02331e9b7febbf8b2258e4f1e1
Popsyynn <popsyynn@gmail.com>
2024-10-07 07:42:03 -0700

    remodified the end points

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
 import com.example.entity.Employee;
+import com.example.exception.EmployeeAlreadyExist;
 import com.example.exception.EmployeeException;
+import com.example.exception.EmployeeNotFoundException;
 import com.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +21,7 @@ public class EmployeeController {
     EmployeeService service;
 
     @RequestMapping(value = "/employee" ,method = RequestMethod.POST)
-    public ResponseEntity saveEmployee(@RequestBody Employee employee) throws EmployeeException {
+    public ResponseEntity saveEmployee(@RequestBody Employee employee) throws EmployeeAlreadyExist {
         service.createEmployee(employee);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
@@ -29,21 +32,21 @@ public class EmployeeController {
         return new ResponseEntity<>(employee , HttpStatus.FOUND);
     }
 
-    @RequestMapping(value = "/employee/{id}",method = RequestMethod.GET)
-    public ResponseEntity<Object> getEmployeeById(@PathVariable Long id) throws EmployeeException {
-        Optional<Employee> employee = service.getEmployeeById(id);
+    @RequestMapping(value = "/employee/email",method = RequestMethod.GET)
+    public ResponseEntity<Object> findEmployeeByEmail(@RequestParam String email) throws EmployeeNotFoundException {
+        Optional<Employee> employee = service.findEmployeeByEmail(email);
         return new ResponseEntity<>(employee , HttpStatus.FOUND);
     }
 
-    @RequestMapping(value = "/employee/{id}" , method = RequestMethod.DELETE)
-    public ResponseEntity<Object> deleteEmployee(@PathVariable Long id) throws EmployeeException {
-        service.deleteEmployee(id);
+    @RequestMapping(value = "/employee" , method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteEmployee(@RequestParam String email) throws EmployeeNotFoundException {
+        service.deleteEmployee(email);
         return new ResponseEntity<>("deleted" , HttpStatus.FOUND);
     }
 
-    @RequestMapping(value = "/employee/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Object> updateEmployee(@PathVariable Long id , @RequestBody Employee employee) throws EmployeeException {
-        service.updateEmployee(id ,employee);
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> updateEmployee(@RequestParam String email , @RequestBody Employee employee) throws EmployeeNotFoundException {
+        service.updateEmployee(email ,employee);
         return new ResponseEntity<>("updated successfully" , HttpStatus.OK);
     }
 }
